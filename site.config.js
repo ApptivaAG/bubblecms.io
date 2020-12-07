aacd2d8917d1a1c5ca2a1b873e71f7d2107457d2
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2020-12-07 16:16:18 +0100

    Format with prettier

--- site.config.js
+++ site.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   build: {
-    srcPath: "./src",
-    outputPath: "./public"
+    srcPath: './src',
+    outputPath: './public',
   },
   site: {
-    title: "Bubble CMS"
-  }
-};
+    title: 'Bubble CMS',
+  },
+}
--- src/assets/utils/play-video-in-viewport.js
+++ src/assets/utils/play-video-in-viewport.js
@@ -7,44 +7,47 @@ js to play video when in viewport; pause when not.
 https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/
 */
 
-const vid = document.getElementById("example-vid");
+const vid = document.getElementById('example-vid')
 
 const isInViewport = function (elem) {
-	let distance = elem.getBoundingClientRect();
-	return (
-		distance.top <= (window.innerHeight - (distance.height / 3) || document.documentElement.clientHeight - (distance.height / 2)) &&
-		distance.left >= 0 &&
-		distance.bottom >= distance.height / 2 &&
-		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
-	);
-};
+  let distance = elem.getBoundingClientRect()
+  return (
+    distance.top <=
+      (window.innerHeight - distance.height / 3 ||
+        document.documentElement.clientHeight - distance.height / 2) &&
+    distance.left >= 0 &&
+    distance.bottom >= distance.height / 2 &&
+    distance.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  )
+}
 
-window.addEventListener("scroll", function (event) {
-	if (isInViewport(vid)) {
-		vid.play()
-	} else {
-		vid.pause()
-	}
-});
+window.addEventListener('scroll', function (event) {
+  if (isInViewport(vid)) {
+    vid.play()
+  } else {
+    vid.pause()
+  }
+})
 
 /* set webp img as hero-background if possible */
 
-function setElementBackgroundImg(element, img1, img2){
-	let img = new Image();
+function setElementBackgroundImg(element, img1, img2) {
+  let img = new Image()
 
-	img.onload = function(){
-		element.style.backgroundImage = "url("+img1+")"
-	};
+  img.onload = function () {
+    element.style.backgroundImage = 'url(' + img1 + ')'
+  }
 
-	img.onerror = function(){
-		element.style.backgroundImage = "url("+img2+")"
-	};
-	
-	img.src = img1;
+  img.onerror = function () {
+    element.style.backgroundImage = 'url(' + img2 + ')'
+  }
+
+  img.src = img1
 }
 
 setElementBackgroundImg(
-	document.getElementsByClassName("hero")[0],
-	"/images/background.webp",
-	"/images/background.jpg"
+  document.getElementsByClassName('hero')[0],
+  '/images/background.webp',
+  '/images/background.jpg'
 )
